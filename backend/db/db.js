0774d56d0cb95e0833096f49f5045d3f096bd599
Nathalia Milani dos Santos <nathmilani2@gmail.com>
2024-05-01 13:55:12 +0200

    Added docker compose files

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,20 +1,24 @@
-const { Sequelize } = require('sequelize');
-
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 const connUrl =
-    process.env.DB_DIALECT + '://' +
-    process.env.DB_USER + ':' +
-    process.env.DB_PW + '@' +
-    process.env.DB_HOST + '/' +
-    process.env.DB_NAME;
+  process.env.DB_DIALECT +
+  "://" +
+  process.env.DB_USER +
+  ":" +
+  process.env.DB_PW +
+  "@" +
+  process.env.DB_HOST +
+  "/" +
+  process.env.DB_NAME;
+console.log("connUrl");
+
+console.log(connUrl);
 
 const db = new Sequelize(connUrl);
-
-const models = [
-    require('../models/todo'),
-];
+const models = [require("../models/todo")];
 
 for (const model of models) {
-    model(db);
+  model(db);
 }
 
 module.exports = db;
