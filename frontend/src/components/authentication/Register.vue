1e50a5d26a74df8c8e6ad06b9c852dc28a2190e7
Nathalia Milani dos Santos <nathmilani2@gmail.com>
2024-06-14 21:32:01 +0200

    Added posthog

--- frontend/src/components/authentication/Register.vue
+++ frontend/src/components/authentication/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Register</h1>
+    <h1>{{ titleText }}</h1>
     <form @submit.prevent="register">
       <input type="text" v-model="username" placeholder="Username" required />
       <input
@@ -9,20 +9,37 @@
         placeholder="Password"
         required
       />
-      <button type="submit">Register</button>
+      <button :style="{ backgroundColor: color }" type="submit">
+        Register
+      </button>
     </form>
     <router-link to="/login">Already have an account? Login</router-link>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import posthog from "posthog-js";
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import { authRegister } from "./../../api";
 
 const username = ref("");
 const password = ref("");
 const router = useRouter();
+const titleText = ref(""); // Default title
+const color = ref(""); // Default color
+
+posthog.setPersonPropertiesForFlags({
+  $browser: Math.random() > 0.5 ? "Chrome" : "Not Chrome",
+});
+
+onMounted(async () => {
+  posthog.onFeatureFlags(() => {
+    const isDarkMode = posthog.getFeatureFlag("theme") === "dark";
+    titleText.value = isDarkMode ? "Sign up now!" : "Join our community!";
+    color.value = isDarkMode ? "white" : "green";
+  });
+});
 
 const register = async () => {
   try {
